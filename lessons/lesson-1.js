8da1acc08ddaa9c04bf93f13fd7de8f50d990e39
Anton_Yashchenko <151663096+AntoXa32@users.noreply.github.com>
2024-11-25 13:25:10 +0200

    add hw-1

---
+++ lessons/lesson-1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// function multiply(x, y, z) {
+//   console.log(`Result: ${x * y * z}`);
+//   return x + y + z;
+// }
+
+// console.log("Log before multiply execution");
+// multiply(2, 3, 5); // "Result: 30"
+// console.log("Log after multiply execution");
---
+++ lessons/lesson-2.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// function isNumberInRange(start, end, number) {
+//   return number >= start && number <= end;
+// }
+
+// console.log(isNumberInRange(10, 5, 4));
+
+// function normalizeInput(input, to) {
+//   return to === "upper" ? input.toUpperCase() : input.toLowerCase();
+// }
+
+// console.log(normalizeInput("Big SALE", "lower"));
+
+// function checkForName(fullName, firstName) {
+//   const loverFullName = fullName.toLowerCase();
+//   const loverFirstName = firstName.toLowerCase();
+//   return loverFullName.includes(loverFirstName);
+// }
+
+// console.log(checkForName("Jason Neis", "Jason"));
+
+// function getFileName(file) {
+//   const dotIndex = file.lastIndexOf(".");
+//   if (dotIndex === -1) {
+//     return file;
+//   } else {
+//     return file.slice(0, dotIndex);
+//   }
+// }
+
+// console.log(getFileName("index.css"));
+
+// function createFileName(name, ext) {
+//   const nameOll = name.trim();
+//   return `${nameOll}.${ext}`;
+// }
+
+// console.log(createFileName(" order ", "txt"));
+
+// function calculateTotal(number) {
+//   let total = 0; // Змінна для зберігання суми
+//   let i = 1; // Лічильник, який починається з 1
+
+//   while (i <= number) {
+//     // Виконуємо цикл, доки i не перевищує number
+//     total += i; // Додаємо поточне значення i до total
+//     i++; // Збільшуємо лічильник
+//   }
+
+//   return total; // Повертаємо загальну суму
+// }
+
+// console.log(calculateTotal(18));
+
+// function calculateTotal(number) {
+//   let total = 0;
+//   for (let index = 1; index <= number; index++) {
+//     total += index;
+//   }
+//   return total;
+// }
+
+// console.log(calculateTotal(18));
