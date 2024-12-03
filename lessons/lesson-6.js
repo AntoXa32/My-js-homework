a18214a9497f8a6f08c8b8e91310dc569b49be1e
Anton_Yashchenko <151663096+AntoXa32@users.noreply.github.com>
2024-12-03 19:47:01 +0200

    add hw-6

--- lessons/lesson-6.js
+++ lessons/lesson-6.js
@@ -1,1 +1,42 @@
 "use strict";
+
+// function greet(str) {
+//   console.log(`${str}, ${this.username}, your room is ${this.room}!`);
+// }
+
+// const mango = {
+//   username: "Mango",
+//   room: 27,
+// };
+
+// const poly = {
+//   username: "Poly",
+//   room: 191,
+// };
+
+// greet.call(mango, "Welcome");
+// greet.call(poly, "Welcome");
+
+// console.log(this);
+
+// class User {
+//   constructor(params) {
+//     this.name = params.name;
+//     this.email = params.email;
+//   }
+
+//   getEmail() {
+//     return this.email;
+//   }
+
+//   changeEmail(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Mango",
+//   email: "mango@mail.com",
+// });
+
+// console.log(User.prototype); // “mango@mail.com”
