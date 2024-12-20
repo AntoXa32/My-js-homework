a092e9c2d11a0c2ca1633a45018d0a710c391e71
Anton_Yashchenko <151663096+AntoXa32@users.noreply.github.com>
2024-12-20 14:46:38 +0200

    add 9-lesson

--- dop-lessons/code.js
+++ dop-lessons/code.js
@@ -30,19 +30,53 @@
 //   refs.passwordButton.textContent = "Розкрити";
 // }
 
-const refs = {
-  statList: document.querySelector(".statList"),
-  resultSection: document.querySelector("#resultSection"),
-  resultButton: document.querySelector("#resultButton"),
-};
-let sum = 0;
-const statButtonClicks = {};
-refs.statList.addEventListener("click", onStatListButtonClick);
+// const refs = {
+//   statList: document.querySelector(".statList"),
+//   resultSection: document.querySelector("#resultSection"),
+//   resultButton: document.querySelector("#resultButton"),
+// };
+// let sum = 0;
+// const statButtonClicks = {};
+// refs.statList.addEventListener("click", onStatListButtonClick);
 
-function onStatListButtonClick(event) {
-  const number = Number(event.target.dataset.number);
-  sum += number;
-  statButtonClicks[event.target.textContent] =
-    (statButtonClicks[event.target.textContent] || 0) + 1;
-  console.log(statButtonClicks);
-}
+// function onStatListButtonClick(event) {
+//   const number = Number(event.target.dataset.number);
+//   sum += number;
+//   statButtonClicks[event.target.textContent] =
+//     (statButtonClicks[event.target.textContent] || 0) + 1;
+//   console.log(statButtonClicks);
+// }
+// const settings = {
+//   theme: "dark",
+//   isAuthenticated: true,
+//   options: [1, 2, 3],
+// };
+
+// localStorage.setItem("settings", JSON.stringify(settings));
+
+// const savedSettings = localStorage.getItem("settings");
+// console.log(savedSettings);
+
+// const parsedSettings = JSON.parse(savedSettings);
+// console.log(parsedSettings);
+
+// localStorage.setItem("ui-theme", "dark");
+// localStorage.removeItem("ui-thems");
+
+// localStorage.clear();
+const formEl = document.querySelector(".feedback-form");
+const textarea = formEl.elements.message;
+const localStorageKey = "goit-example-message";
+
+textarea.value = localStorage.getItem(localStorageKey) ?? "";
+
+formEl.addEventListener("input", (event) => {
+  localStorage.setItem(localStorageKey, event.target.value);
+});
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event.target.elements.message.value);
+  localStorage.removeItem(localStorageKey);
+  formEl.reset();
+});
